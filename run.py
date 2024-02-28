6ccbf503d2b79c4d6926a0d5b53197395c88e9f8
Colin Breakell <cbreakell99@gmail.com>
2024-02-28 13:08:49 +0000

    Created main_menu function with a welcome message and option to start the game or view instuctions by a user input field

--- run.py
+++ run.py
@@ -1,3 +1,27 @@
 # Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+
+inventory = []
+
+def main_menu():
+    print("Hello, Welcome to Hollow Halls Murder Mystery Game. \n")
+    start = input("Input 1: - Start Game \nInput 2: - View Instuctions").lower()
+    if start == "1":
+        start_game()
+    elif start == "2":
+        instructions()
+    else: 
+        print("Invalid input - You must enter 1 to start the game or 2 to view instuctions")
+
+def start_game():
+    print("starting game")
+
+
+
+def instructions():
+    print("instructions")
+
+main_menu()
+
+
+
