5ebcb17c3ae6d2305bf4bb02d24e562ad69eb8c2
Colin Breakell <cbreakell99@gmail.com>
2024-03-01 02:29:20 +0000

    Added quit option so far in main menu and start game functions

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 import os
 
-
 # Inventory for items picked up in the game
 inventory = []
 
@@ -22,30 +21,36 @@ clear_screen()
 
 # Main Menu / Start screen function
 def main_menu():
+    clear_screen()
     print("Hello, Welcome to Hollow Halls Murder Mystery Game. \n")
-    start = input("Type 1: - Start Game \n Type 2: - View Instuctions \n").lower()
+    start = input("Type 1: - Start Game \n Type 2: - View Instuctions \n Type 3: - Quit \n").lower()
     if start == "1":
         start_game()   
     elif start == "2":
         instructions()
-    else: 
+    elif start == "3": 
+        quit()
+    else:
         print("Invalid input - You must enter 1 to start the game or 2 to view instuctions")
-
-
+        main_menu()
+        
+        
 # Start Game Function 
 def start_game():
     clear_screen()
     name = input("Please enter your Surname: ").capitalize()
     clear_screen()
-    print(f"Hello Detective.{name}. Welcome to Hollow Halls Private Psychiatric Facility. I see you have arrived earlier then expected, I was just out for a run, please ignore the sweat and heavy breathing haha, I was just about to hop in to the shower. My name is Dr.Mengele, I am head Psychiatist here at Hollow Halls for over 40 years. Thank you for coming, we are happy to have you here. My wife Lauren found the body, full of blood in the main hall just under the stairs. It is a horrible site to see, she is in shock from it, the poor thing, as am I. Lauren said she saw four suspects running away from the scene, she ran after them and saw the suspects make their way down to the cellar and into the testing labs. By the looks of it they were trying to hide or escape the scene and found themselves locked in the room. The suspects triggered the defense alarm and locked the doors from the inside as they did not put in the correct code to turn of the alarm when they ran into the testing labs. I am honestly not sure what the code is to stop the alarms and re-open the doors. Dr.Robertson is away on holidays and wont be back until tomorrow he is head of the testing labs. You might have to come back Tomorrow to talk with the suspects but until then you are free too search the Facility we are in the common room currently, hopefully you find some clues and can catch the killer and arrest them when Dr.Robertson comes back.")
-    decision = input("Ok, time to exlore this facility and see what I can find. \n Do you want to go to the main hall or the living room? (type main hall or living room) \n").lower()
+    print(f"Hello Detective.{name}, Welcome to Hollow Halls Private Psychiatric Facility. I see you have arrived earlier then expected, I was just out for a run, please ignore the sweat and heavy breathing haha, I was just about to hop in to the shower. My name is Dr.Mengele, I am head Psychiatist here at Hollow Halls for over 40 years. Thank you for coming, we are happy to have you here. My wife Lauren found the body, full of blood in the main hall just under the stairs. It is a horrible site to see, she is in shock from it, the poor thing, as am I. Lauren said she saw four suspects running away from the scene, she ran after them and saw the suspects make their way down to the cellar and into the testing labs. By the looks of it they were trying to hide or escape the scene and found themselves locked in the room. The suspects triggered the defense alarm and locked the doors from the inside as they did not put in the correct code to turn of the alarm when they ran into the testing labs. I am honestly not sure what the code is to stop the alarms and re-open the doors. Dr.Robertson is away on holidays and wont be back until tomorrow he is head of the testing labs. You might have to come back Tomorrow to talk with the suspects but until then you are free too search the Facility we are in the common room currently, hopefully you find some clues and can catch the killer and arrest them when Dr.Robertson comes back.")
+    decision = input("Ok, time to exlore this facility and see what I can find. \n Do you want to go to the main hall or the living room? (type main hall or living room) \n Type quit to main menu \n").lower()
     if decision == "main hall":
         main_hall()
     elif decision == "living room":
         living_room()
+    elif decision == "quit": 
+        main_menu()
     else:
         print("Invalid input - You must choose between the main hall or the living room (Type main hall or living room into terminal)")
-
+        clear_screen()
 
 # Common Room function
 def common_room():
