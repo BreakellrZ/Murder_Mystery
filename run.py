32a5717b8d81460a50fc8144f031c1b39534c1d3
Colin Breakell <cbreakell99@gmail.com>
2024-03-04 15:25:42 +0000

    fixed while loop bug in testing_labs function - added .lower to all input fields and finished typing up winner function

--- run.py
+++ run.py
@@ -60,14 +60,14 @@ Hello Detective.{name}, Welcome to Hollow Halls Private Psychiatric Facility.
 I see you have arrived a lot earlier then expected, I was just out for a run,
 please ignore the sweat and heavy breathing haha.
 My name is Dr.Mengele. I am head Psychiatist and Owner of this facility here at Hollow Halls, for over 40 years.
-We house patients under the age of 40 here, and have done so for plenty of years.
+We house patients under the age of 40 here.
 Thank you for coming on such short notice, we are happy to have you here.
 
 Horrible murder today, the body's face is completely smashed - It would be impossible to ID the person.
 It happened around 1:55pm
 Although, one of our patients is not present in their rooms and we have identified that it must be Mr.Hardy.
 He was a lovely person and we are all deeply saddened and shocked by this incident today.
-We have reached about to his family today, to tell them the news. They never visited him.
+We have reached out to his family today, to tell them the news. They never visited him.
 The body is in the main hall, we did not want to move it incase it gives you any clues.
 
 I know you just got here, but this is very important information and I need to share it with you right away.
@@ -79,7 +79,7 @@ They can not come back out. All four suspects are locked in their for you. The o
 I do not run the testing labs I am never down here.
 Dr.Robertson does, and you need a special Key Card to open the doors which only he has. 
 Now, unfortunatly Dr.Robertson is away on Holiday for the past week but he is back tomorrow.
-This will give you a lot of time to be able to put the pieces togehter and hopefully solve this brutal Murder.
+This will give you a lot of time to be able to put the pieces together and hopefully solve this brutal Murder.
 You are free to move around Hallow Halls as much as you want to gather evidence, 
 and come to your conclusion of WHO MURDERED MR.HARDY.
 """)
@@ -181,7 +181,7 @@ or back to the common room?
                 else: 
                     clear_screen()
                     clues()
-                    print("Invalid input - please type 'bedroom' or 'common room' into the terminal")
+                    print(" \n Invalid input - please type 'bedroom' or 'common room' into the terminal \n")
         elif decision == "bedroom":
             bedroom()
             break
@@ -204,8 +204,8 @@ Ms.Mengele: 'Hello Detective, It was not actually me you called it was our maid,
 I sent her home as she was in a lot of shock.
 You are very welcome here at the wealthiest Pyschiatric Facility in Europe.
 Please do ask any questions you may have about the suspects. I do hope Dr.Robertson arrives back tomorrow,
-so you can put away the murderer, bring peace back to Hallow Halls and to Mr.Hardys family.'""")
-        decision = input("""Do you want to ask Ms.Mengele about the suspects,
+so you can put away the murderer, bring peace back to Hallow Halls and to Mr.Hardys family.\n'""")
+        decision = input("""\nDo you want to ask Ms.Mengele about the suspects,
 or go to Dr.Robertson's Room?
 \n(Type suspects or Dr.Robertson) \n""" ).lower()
         if decision == "suspects":
@@ -227,6 +227,7 @@ if you know what I mean.
 She has a history of drug abuse,
 she is also best of friends with Josh they never leave eachothers sides.
 They share a bedroom down the hall. 
+She also had beef with Mr.Hardy!!
             
 Nicki, well suprised me the most she is quite and calm,
 but you know detective sometimes the quite ones can do the most harm, she lives upstairs and has ocd everthing is
@@ -240,7 +241,7 @@ You: All interesting points there. I should try and remember this conversation."
         
             while True:
                 decision2 = input("""\nGo to Alice and Joshs room or back to common room \n
-(Type Alice and josh or common room\n)""")
+(Type Alice and josh or common room) \n""").lower()
                 if decision2 == "alice and josh":
                     alice_josh()
                     break
@@ -353,9 +354,9 @@ To your right you see the doors in which the suspects are locked in. You need th
             print("""\nYou make your way over to the safe.
 This looks tricky... it looks like I need to type in a word.\n""")
             while True:
-                decision2 = input("""\nInput the word you think is needed to open the safe\n""")
+                decision2 = input("""\nInput the word you think is needed to open the safe\n""").lower()
                 if decision2 == "piano":
-                    print("""Looks like a diary, lets take a look.
+                    print("""\nLooks like a diary, lets take a look.
 'Date march 5th 2024.
 Time of day: 7pm.
 Today marks the most important day in my life, in fact mabye one of the most important days in human history.
@@ -368,27 +369,27 @@ but I hope they can see the bigger picture and realize that this is what is best
 I am excited to tell the world tomorrow.""")
                     if "Dr.Robertson's Discovery, Spare Key card" not in inventory:
                         inventory.append("Dr.Robertson's Discovery, Spare Key card")
-                    decision3 = input("""WOW THIS IS VERY IMPORTANT - Lots of clues in that diary.
+                    while True:
+                        decision3 = input("""WOW THIS IS VERY IMPORTANT - Lots of clues in that diary.
 hmm ... some things just dont quite add up ...Time to make my way back to the common room.
 I need to put all my clues together.
 I think I know what happened here at Hallow Hall, after I come to a conclusion lets go see the suspects.\n
-Type (common room)\n""")
-                    
-                    if decision3 == "common room":
-                        common_room()
-                        break
-                    elif decision3 == "quit":
-                        quit()
-                    else:
-                        clear_screen()
-                        clues()
+Type (common room)\n""").lower() 
+                        if decision3 == "common room":
+                            common_room()
+                            break
+                        elif decision3 == "quit":
+                            quit()
+                        else:
+                            clear_screen()
+                            clues()
                         print("\nInvalid input - Type common room\n")
                 elif decision2 == "quit":
                     quit()
                 else:
                     while True:
-                        print("Damn looks like I got it wrong, mabye I can find a clue for this somewhere")
-                        wrong = input("Type common room to go back and search for more clues")
+                        print("Damn looks like I got it wrong, mabye I can find a clue for this somewhere \n")
+                        wrong = input("Type common room to go back and search for more clues \n").lower()
                         if wrong == "common room":
                             common_room()
                             break
@@ -448,11 +449,107 @@ All four suspects look heavily sedated and dazed... as expected...
 
 def winner():
     clear_screen()
-    print("well done")
+    print("""IT WAS YOU DR.MENGELE !!
+
+You might be asking how did I figure it out? Well let me explain.....
+
+First of all, You said you were out for a 'run', you were sweating, and out of breathe.
+You were shocked by my early arrival, the only reason you were sweating was because you were in a rush to cover up this murder after you heard the maid call me.
+When you were told I arrived earlier then expected you were shocked and ran around as quickly as possible to try and cover this all up but you failed.
+This was the first lie of many. In fact you were running around trying to frame these four 'suspects' which you had locked
+in the basement because they saw you murderer this victim.
+
+Forty years here huh? You are quite old.. Nice set of grey hair you have Dr. Almost as if the only piece of hair on the victims body was infact grey,
+and none of your 'suspects' have grey hair? You mentioned you did not go near or move the body? How would the grey hair get there then? The victim was aslso bald.
+
+Now... you trapped them 'suspects' in there Dr, they did not run in there themselves. You drugged them after you saw them see you murder this victim.
+As you can tell by how drugged up they all look right now you used some sort of amnesia drug so that they would forget about all this in the morning.
+You had a keycard this whole time because your one is the exact same one I found to unlock this door.
+You had them trapped in here because you knew they could not escape from the inside. But this whole time you were just lying to me about not being able to open the door,
+and that only Dr.Robertson had a special keycard. You are the owner for christ sake of course you have a keycard. You wanted me to stay the night,
+so that you could come up with a plan on what to do with these four 'suspects' during the night ... mabye you were going to kill them all and stage an 'escape'??
+You needed them trapped so you could frame them one way or another and you needed time to think and come up with a plan as I got here very quickly.
+
+I must say you done a fairly good job in trying to frame these suspects but you missed some key details which let me to this discovery that it was you and not them.
+
+You moved the body to the bottom of the stairs, this is where the grey hair came from. You then tried to frame Tom. You knew you had stabbed the victim in the back.
+You knew I would see the stab wounds and you thought I would think that Tom was actually the one doing the framing. You thought I would think that Tom placed the body,
+at the bottom of the stiars, so that it looked like someone pushed the body to the stairs to his death. Of course Tom can not walk up the stairs and you used his disability
+as a chance for me to think he was the one doing the framing. When in fact is was all you. Now of course it still could have been Tom..
+but ...It was not Tom because the actual murder weapon was upstairs !! There would have been no way Tom could have stashed the Murder weapon up there. So that rules him out.
+
+Now you ask what about Nicki? Nicki has OCD which your wife told me. Everything in Nickis rooms is super clean. I found blood droplets in the sink.. How would she has missed this?
+Ah yes, because it was not her... it was you in a rush trying to clean the blood from the knife, you left some droplets in the sink because you were in such a rush.
+You then tried to 'hide' the knife. But really you wanted me to see it, you wanted me to mabye think it was Nicki who murdered the victim. but again DR.
+you made a mistake. It could not have been Nicki because you placed it in a really high place, Nicki could never reach that she is only 5ft.
+
+Your wife is not much better. She was filling me with some lies too... She was trying to set up Alice telling me she was a bad drug user and that she could get crazy.
+She also said she had 'beef' with Mr.Hardy the alledged victim. Now I am not sure if she had beef with Mr.Hardy but I do not care about that and I will explain later,
+but Alice was not a drug user inface Alice was the opposite - Alice was part of the straight edge society which means she never done hardcore drugs in her life.
+
+Josh and Alice were also togehter after the murder Josh was drawing a pciture of his favourite comic book heros this would have taken hours.
+Josh and Alice sign the back of every single picture Josh drew with a date and time of completion.
+There is no way they could have commited this murder because they were in there rooms at the time. You said the 'suspects' ran to the testing labs.
+This is not true they must have heard the maids screem of seeing the body in the main hall and you saw them looking out of there rooms and drugged them and brought them down.
+
+So it was not Tom, Nicki, Alice, or Josh. IT WAS YOU ! 
+
+Now.... back to explaining why I did not care that Alice may or may not have had beef with Mr.Hardy... Mr.hardy has not been on your records for months..
+I presume you may have killed him too... as he is no where to be found for months from your records and his family never visits him.
+ He is an easy pateint to pretend that this is his body, no one would question it, not even the family. 
+
+That body IS NOT MR.HARDY'S BODY! THAT BODY IS DR.ROBERTSON'S BODY!!
+
+You killed Dr.Robertson because of what he had discovered!
+You knew it would destroy your wealth!! 
+He was about to save millions of lives and you murdered him in cold blood because of how selfish you are.
+You are a sick man Dr.Mengele - Luckiliy I have figured it out and I will get Dr.Robertsons work in the hands of the right people.
+
+You destroyed Dr.Robertsons face because you did not want him ID'd - You were going to dispose of his body during the night.
+You used an excuse that he was on Holiday. You were going to use that as an excuse as to why he never returned.... and must have died over there.
+I read his diary. He was here YESTERDAY  Dr.... You told me he has been gone for a week on Holiday... 
+
+There was some piece of ripped clothing down here I found near the testing labs... It was his.
+You stabbed him down here after he told you about the discovery he made. You could not let this happen. 
+You stabbed him and a piece of his clothing came off with it, his body when I searched it in the main hall has a peice of clothing missing from his back.
+You moved him up to the main hall, the maid saw the body and called me. You then had to quickly come up with a solution to frame someone else and get any
+suspicion away from you.
+He also had a pair of glasses and a testing tube in his pocket. You really should have taken them out Dr.Mengele... but I know you were in a rush.
+Interesting how Dr.Robertson had some unopened contact lenses in his room. He must still stick to wearing his glasses...
+The testing tube is a bit of a giveaway I dont think I need to explain that.
+
+So there we have it... YOU MURDERED DR.ROBERTSON. 
+
+“You have the right to remain silent. Anything you say can and will be used against you in a court of law.
+You have the right to an attorney. If you cannot afford an attorney, one will be provided for you.
+Do you understand the rights I have just read to you?”
+
+..........................
+
+CONGRATULATIONS YOU HAVE SOLVED THE HALLOW HALLS MURDER MYSTERY WELL DONE !! \n
+GAME OVER.
+""")
+    while True:
+        ending = input("Type quit to quit the game")
+        if ending == "quit":
+            quit()
+        else:
+            clear_screen()
+            print("Invalid input type quit")
 
 def loser():
     clear_screen()
-    print("ooh noo game over")
+    while True:
+        print("Damn you got it wrong... try again next time... YOU JUST SENT AN INNOCENT PERSON TO PRISON HOW COULD YOU!!")
+        ending = input("Type restart to restart the game").lower()
+        if ending == "restart":
+            clear_screen
+            main_menu()
+            break
+        elif ending == "quit":
+            quit()
+
+
 
 # Bedroom
 def bedroom():
@@ -485,13 +582,13 @@ Do you want to have a look in there or do you want to make your way back to the 
 what is that?
 
 Oh wow, uhh jesus there looks like there is some drops of blood in the sink..
-This must be where the killer sleeps this must be the killer..
+This may be where the killer sleeps this may be the killer..
 It looks like they were in a rush and did not clean properly.
 
 - Ok lets make my way back downstairs and into the common room to re-asses. \n""")
 
             while True:
-                home = input("\nTime to make my way back to the Common Room (Type Common Room to get there) \n")
+                home = input("\nTime to make my way back to the Common Room (Type Common Room to get there) \n").lower()
                 if home == "common room":
                     common_room()
                     break
@@ -499,6 +596,7 @@ It looks like they were in a rush and did not clean properly.
                     quit()
                 else:
                     clear_screen()
+                    clues()
                     print("Invalid input - please type in common room to the terminal\n") 
 
         elif decision == "common room":
